55cfc50c695bc6773e624a448967a103bd108449
Dawson Ibrahim Massam <71150010+Dawinz@users.noreply.github.com>
2025-06-11 16:03:09 +0300

    Enhance scheduler with multi-participant meetings and lawyer editing

--- includes/email-functions.php
+++ includes/email-functions.php
@@ -1,25 +1,24 @@
 <?php
 require_once HILTON_PLUGIN_DIR . 'includes/graph-handler.php';
 
-function hilton_send_meeting_email($client_email, $lawyer_email, $meeting_data) {
-    $subject_client = "Your Meeting with Hilton Attorneys";
-    $subject_lawyer = "You Have a New Meeting Scheduled";
+function hilton_send_meeting_email($client_emails, $lawyer_emails, $meeting_data) {
+    $subject = 'Meeting Scheduled: ' . $meeting_data['title'];
     $headers = ['Content-Type: text/html; charset=UTF-8', 'From: Hilton Attorneys <info@hiltonlawgroup.co.tz>'];
 
     // Create Teams meeting if not already created
-    if (empty($meeting_data['meeting_link'])) {
+    if ($meeting_data['meeting_type'] === 'online' && empty($meeting_data['meeting_link'])) {
         $start_utc = date('c', strtotime($meeting_data['datetime']));
         $end_utc = date('c', strtotime($meeting_data['datetime'] . ' +1 hour'));
-        $meeting_link = hilton_create_teams_meeting($start_utc, $end_utc, 'Meeting with ' . $meeting_data['client_name']);
-    } else {
-        $meeting_link = $meeting_data['meeting_link'];
+        $meeting_data['meeting_link'] = hilton_create_teams_meeting($start_utc, $end_utc, $meeting_data['subject']);
     }
 
     $message = file_get_contents(HILTON_PLUGIN_DIR . 'templates/emails/meeting-invite.php');
-    $message = str_replace('{{client_name}}', esc_html($meeting_data['client_name']), $message);
-    $message = str_replace('{{meeting_time}}', esc_html($meeting_data['datetime']), $message);
-    $message = str_replace('{{meeting_link}}', esc_url($meeting_link), $message);
+    foreach ($meeting_data as $key => $value) {
+        $message = str_replace('{{' . $key . '}}', esc_html($value), $message);
+    }
 
-    wp_mail($client_email, $subject_client, $message, $headers);
-    wp_mail($lawyer_email, $subject_lawyer, $message, $headers);
+    $recipients = array_merge((array)$client_emails, (array)$lawyer_emails);
+    foreach ($recipients as $email) {
+        wp_mail($email, $subject, $message, $headers);
+    }
 }
